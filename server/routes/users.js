9be8f4bff1eb80adf5f1fb0a6d5ff51d4e43dd90
BasharBakri <basharbakriwork@gmail.com>
2023-03-25 14:34:54 +0300

    some error adjusting, probably not as good

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 // Get all users
 //  @route GET /users
+// table: username, passportId, cash, credit, active
 
 const getUsers = async (req, res, next) => {
 
@@ -27,6 +28,8 @@ router.route('/').get(getUsers);
 // 2. When fetching users, make sure they exist wtf does that mean
 // no duplicates allowed 
 // default 0 cash 0 credit
+// form Input in client: {username: string}
+// navigate to all users table after form submit
 
 const addUser = async (req, res, next) => {
   console.log(req.body);
@@ -45,17 +48,12 @@ const addUser = async (req, res, next) => {
 
   }
 };
-
-
-
-
-
 router.route('/').post(addUser);
 
 
 // view single user
 //  @route get /users/:id
-// details: username, passportId, cash, credit, isActive, 
+// row: username, passportId, cash, credit, isActive.
 
 const getOneUser = async (req, res, next) => {
 
@@ -63,12 +61,15 @@ const getOneUser = async (req, res, next) => {
     const user = await UserSchema.findById(req.params.id);
     console.log(req.params.id);
     if (!user) {
-      return res.status(404).json({ success: false });
+      // return res.status(404).json({ success: false });
+      return next(error);
+
     }
     res.status(200).json({ success: true, data: user });
 
   } catch (error) {
-    res.status(400).send(error);
+    // res.status(400).send(error);
+    next(error);
   }
 };
 
@@ -78,7 +79,8 @@ router.route('/:id').get(getOneUser);
 
 // deposit  single user
 //  @route get /users/:id/deposit
-// Only number and positive deposit, limit by 10k maybe?, 
+// Only number and positive deposit, limit by 10k maybe?
+// links: deposite, update, withdraw, transfer.
 
 const depositOneUser = async (req, res, next) => {
   try {
@@ -86,7 +88,8 @@ const depositOneUser = async (req, res, next) => {
     const amount = req.body.amount;
 
     if (!user || typeof amount !== 'number' || amount < 1 || amount > 50000) {
-      return res.status(400).json({ success: false });
+      // return res.status(400).json({ success: false });
+      return next(error);
     }
 
 
@@ -94,7 +97,7 @@ const depositOneUser = async (req, res, next) => {
     await user.save();
     res.status(200).json({ success: true, amount: amount, balance: user.cash, max: 50000 });
   } catch (error) {
-    res.status(400).send(error);
+    next(error);
   }
 };
 router.route('/:id/deposit').put(depositOneUser);
@@ -103,20 +106,23 @@ router.route('/:id/deposit').put(depositOneUser);
 // addCredit single user
 //  @route get /users/:id/addCredit
 // Only number and positive addCredit, limit by 10k maybe?
+// navigate to /:id
 
 const addCreditOneUser = async (req, res, next) => {
   try {
     const user = await UserSchema.findById(req.params.id);
     const amount = req.body.amount;
     if (!user || typeof amount !== 'number' || amount < 1 || amount > 50000) {
-      return res.status(400).json({ success: false });
+      // return res.status(400).json({ success: false });
+      return next(error);
     }
 
     user.credit += amount;
     await user.save();
     res.status(200).json({ success: true, amount: amount, credit: user.credit, max: 50000 });
   } catch (error) {
-    res.status(400).send(error);
+    // res.status(400).send(error);
+    next(error);
   }
 };
 
@@ -128,6 +134,8 @@ router.route('/:id/addCredit').put(addCreditOneUser);
 //  @route get /users/:id/withdraw
 // Only number, remove cash first from user. If it reaches 0, then remove credit until it also reaches 0
 // prevent if both cash and credit are 0
+// navigate to /:id
+
 
 const withdrawMoneyOneUser = async (req, res, next) => {
   try {
@@ -153,7 +161,8 @@ const withdrawMoneyOneUser = async (req, res, next) => {
 
 
   } catch (error) {
-    res.status(400).send(error);
+    // res.status(400).send(error);
+    next(error);
   }
 };
 
@@ -166,6 +175,8 @@ router.route('/:id/withdraw').put(withdrawMoneyOneUser);
 // Only number, remove cash first from user. If it reaches 0, then remove credit until it also reaches 0
 // prevent if both cash and credit are 0
 // select user by username to increase their cash
+// navigate to /:id
+
 
 const tranferFromOneUser = async (req, res, next) => {
   try {
@@ -197,7 +208,8 @@ const tranferFromOneUser = async (req, res, next) => {
 
 
   } catch (error) {
-    res.status(400).send(error);
+    // res.status(400).send(error);
+    next(error);
   }
 };
 
